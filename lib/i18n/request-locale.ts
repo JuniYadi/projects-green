fbf5895264b57a8a1b6847e6a7d142a0f3ad11c6
JuniYadi <juniyadi94@gmail.com>
2026-05-20 07:21:39 +0700

    feat: add request locale resolver

---
+++ lib/i18n/request-locale.ts
@@ -0,0 +1,53 @@
+import { match as matchLocale } from "@formatjs/intl-localematcher"
+import Negotiator from "negotiator"
+
+import {
+  defaultLocale,
+  locales,
+  type AppLocale,
+} from "@/lib/i18n/config"
+import { isLocale } from "@/lib/i18n/pathname"
+
+const resolveFromHeader = (acceptLanguageHeader: string | undefined) => {
+  if (!acceptLanguageHeader) {
+    return defaultLocale
+  }
+
+  const requestedLocales = new Negotiator({
+    headers: {
+      "accept-language": acceptLanguageHeader,
+    },
+  })
+    .languages()
+    .filter((language) => language !== "*")
+
+  if (requestedLocales.length === 0) {
+    return defaultLocale
+  }
+
+  try {
+    return matchLocale(requestedLocales, locales, defaultLocale)
+  } catch {
+    return defaultLocale
+  }
+}
+
+export const resolveRequestLocale = ({
+  acceptLanguageHeader,
+  cookieLocale,
+}: {
+  acceptLanguageHeader: string | undefined
+  cookieLocale: string | undefined
+}): AppLocale => {
+  if (cookieLocale && isLocale(cookieLocale)) {
+    return cookieLocale
+  }
+
+  const matchedLocale = resolveFromHeader(acceptLanguageHeader)
+
+  if (isLocale(matchedLocale)) {
+    return matchedLocale
+  }
+
+  return defaultLocale
+}
