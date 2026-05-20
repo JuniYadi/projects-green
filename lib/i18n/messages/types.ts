ae0af9a26f37829eec637096761a5c8434e39886
JuniYadi <juniyadi94@gmail.com>
2026-05-20 07:21:39 +0700

    feat: define i18n message types

---
+++ lib/i18n/messages/types.ts
@@ -0,0 +1,18 @@
+export type AppMessages = {
+  navUser: {
+    label: string
+    languageLabel: string
+    languageMenuLabel: string
+    languages: {
+      en: string
+      id: string
+    }
+    menu: {
+      upgrade: string
+      account: string
+      billing: string
+      notifications: string
+      logout: string
+    }
+  }
+}
