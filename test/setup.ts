4a8b58bd0824f9899bbec3c28628dd272e809a6c
JuniYadi <juniyadi94@gmail.com>
2026-05-23 22:03:11 +0700

    fix: use path alias for register import in test setup

--- test/setup.ts
+++ test/setup.ts
@@ -1,4 +1,4 @@
-import "./register"
+import "@/test/register"
 import { afterEach, expect } from "bun:test"
 import { cleanup } from "@testing-library/react"
 import * as matchers from "@testing-library/jest-dom/matchers"
