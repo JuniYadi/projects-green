32374cd5e55c6b3b493d93c1da4d9edd54441aa8
JuniYadi <juniyadi94@gmail.com>
2026-06-03 23:22:42 +0700

    feat: add PUT/PATCH/DELETE HTTP method exports to API catch-all route
    Support all standard HTTP methods through the Elysia catch-all route.

--- app/api/[[...slugs]]/route.ts
+++ app/api/[[...slugs]]/route.ts
@@ -5,3 +5,6 @@ export const dynamic = "force-dynamic"
 
 export const GET = app.fetch
 export const POST = app.fetch
+export const PUT = app.fetch
+export const PATCH = app.fetch
+export const DELETE = app.fetch
