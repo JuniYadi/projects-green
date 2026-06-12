fd75116f7ff68d7a742cc29a1a597bba6ac42924
JuniYadi <juniyadi94@gmail.com>
2026-06-13 03:59:06 +0700

    fix(test): scope bun unit test discovery

--- test/layout-test-mocks.ts
+++ test/layout-test-mocks.ts
@@ -1,3 +1,22 @@
+export const createNavigationMock = ({
+  pathname,
+  redirect,
+}: {
+  pathname: string
+  redirect: (path: string) => void
+}) => {
+  return {
+    redirect,
+    usePathname: () => pathname,
+    useSearchParams: () => new URLSearchParams(),
+    useRouter: () => ({
+      replace: () => {},
+      push: () => {},
+      refresh: () => {},
+    }),
+  }
+}
+
 export const createAuthMock = ({
   withAuth,
   getUser,
