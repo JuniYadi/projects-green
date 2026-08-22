383b790582386046aac189f1d96094a40baafc6a
Juni Yadi <juniyadi94@gmail.com>
2026-08-23 01:57:46 +0700

    fix(config): default APP_NAME to PFNApp

--- lib/app-config.ts
+++ lib/app-config.ts
@@ -3,4 +3,4 @@
  * Environment variables prefixed with NEXT_PUBLIC_ are inlined at build time
  * and available on both server and client.
  */
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ?? "Acme Inc."
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ?? "PFNApp"
