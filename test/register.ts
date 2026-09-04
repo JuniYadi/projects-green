62f7bbba553751acc098df832ca37e5741f44746
JuniYadi <31454084+JuniYadi@users.noreply.github.com>
2026-09-05 03:19:30 +0700

    revert: revert unexpected change to test/register.ts

--- test/register.ts
+++ test/register.ts
@@ -3,5 +3,3 @@ import "@testing-library/jest-dom/matchers"
 import { GlobalRegistrator } from "@happy-dom/global-registrator"
 
 GlobalRegistrator.register()
-import { mock } from "bun:test"
-mock.module("server-only", () => ({}))
